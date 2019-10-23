89a0b75098c4f5d968565b7db5a12e0f18fc31d0
Shahriar <hello@shahmanmastery.com>
2019-10-23 19:14:56 +0800

    react: call back, default

--- react/introduction/src/App.js
+++ react/introduction/src/App.js
@@ -170,22 +170,137 @@ import { Greet } from "./components/Greet";
 // };
 
 //------------react state, hook, greeting-----------
+// const App = () => {
+//   return <Headline />;
+// };
+
+// const Headline = () => {
+//   const [greeting, setGreeting] = useState("Hello Function Component!");
+//   const handleChange = e => {
+//     setGreeting(e.target.value);
+//   };
+//   return (
+//     <div className="App-header">
+//       <h1>{greeting}</h1>
+
+//       <input type="text" value={greeting} onChange={handleChange} />
+//     </div>
+//   );
+// };
+
+//--------------call back function--------------------
+// IT IS POSSIBLE TO PASS A FUNCTION TO A CHILD COMPONENT
+// const App = () => {
+//   const [greeting, setGreeting] = useState("Hello Function Component");
+
+//   const handleChange = e => setGreeting(e.target.value);
+
+//   return <Headline headline={greeting} onChangeHeadLine={handleChange} />;
+// };
+
+// const Headline = props => {
+//   const headline = props.headline;
+//   const onChangeHeadline = props.onChangeHeadLine;
+
+//   return (
+//     <div className="App-header">
+//       <h1>{headline}</h1>
+
+//       <input type="text" value={headline} onChange={onChangeHeadline} />
+//     </div>
+//   );
+// };
+
+// ---------------------children - call back ---------------------
+// USE {CHILDREN} to pass Set Greeting: to the input component
+// Send to two components
+// const App = () => {
+//   const [greeting, setGreeting] = useState("Hello Function Component");
+
+//   const handleChange = e => setGreeting(e.target.value);
+
+//   return (
+//     <div>
+//       <Headline headline={greeting} />
+
+//       <Input inputValue={greeting} onChangeInput={handleChange}>
+//         Set Greeting:
+//       </Input>
+//     </div>
+//   );
+// };
+
+// const Headline = props => {
+//   const headline = props.headline;
+
+//   return (
+//     <div>
+//       <h1>{headline}</h1>
+//     </div>
+//   );
+// };
+
+// const Input = ({ inputValue, onChangeInput, children }) => (
+//   <label>
+//     {children}
+//     <input type="text" value={inputValue} onChange={onChangeInput} />
+//   </label>
+// );
+
+// --------------------override, using default function---------------
+// const App = () => {
+//   const sayHello = () => console.log("Hello");
+//   return <Button handleClick={sayHello} />;
+//   // return <Button handleClick={null} />;
+// };
+// const Button = ({ handleClick }) => {
+//   const sayDefault = () => console.log("Default");
+//   const onClick = handleClick || sayDefault;
+//   return (
+//     <button type="button" onClick={onClick}>
+//       Button
+//     </button>
+//   );
+// };
+
+// ---------------dummy--------------------
+// const App = () => {
+//   const person = {
+//     name: "Shahrar",
+//     age: 25
+//   };
+
+//   const Print = () => {
+//     console.log(`${person.name} is ${person.age} years' old.`);
+//   };
+
+//   // return <Headline myPerson={person} myPrint={Print} />;
+//   return <Headline myPerson={person} myPrint={null} />;
+// };
+
+// const Headline = ({ myPerson, myPrint }) => {
+//   const defaultPrint = () => console.log("Default");
+//   const handleClick = myPrint || defaultPrint;
+
+//   return (
+//     <div class="App-header">
+//       <button type="button" onClick={handleClick}>
+//         Print Button
+//       </button>
+//     </div>
+//   );
+// };
+
+//-----------------default (es6 method)----------------
+import React from "react";
 const App = () => {
-  return <Headline />;
+  const sayHello = () => console.log("Hello");
+  return <Button handleClick={sayHello} />;
 };
-
-const Headline = () => {
-  const [greeting, setGreeting] = useState("Hello Function Component!");
-  const handleChange = e => {
-    setGreeting(e.target.value);
-  };
-  return (
-    <div className="App-header">
-      <h1>{greeting}</h1>
-
-      <input type="text" value={greeting} onChange={handleChange} />
-    </div>
-  );
-};
+const Button = ({ handleClick = () => console.log("Default") }) => (
+  <button type="button" onClick={handleClick}>
+    Button
+  </button>
+);
 
 export default App;
